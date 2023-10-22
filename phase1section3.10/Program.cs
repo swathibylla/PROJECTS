22a11e9330f61ac8d00a8f920f3bdd70b3a75b3e
swathibylla <swathibylla@gmail.com>
2023-10-22 19:01:32 +0530

    All files

---
+++ phase1section3.10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phase1section3._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            doApp();
+        }
+        static void doApp()
+        {
+            string str = "Hello, ";
+            str += "world!";
+            Console.WriteLine("using string: "+ str);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Hello, ");
+            stringBuilder.Append("world!");
+            string result = stringBuilder.ToString();
+            Console.WriteLine("using StringBuilder:" + result);
+
+        }
+    }
+}
