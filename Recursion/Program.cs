2e671fd03d7ce3c8f9bd8368bf7973cb3850c516
swathibylla <swathibylla@gmail.com>
2023-10-28 23:18:32 +0530

    files

---
+++ Recursion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phase1section4._27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            string[] students = new string[8];
+            students[0] = "Kavya";
+            students[1] = "Mani";
+            students[2] = "Akash";
+            students[3] = "Sandeep";
+            students[4] = "Naveen";
+            students[5] = "Harsha";
+          
+
+            int currElem = 0;
+            printViaRecursion(students, currElem);
+        }
+
+        public static void printViaRecursion(string[] arr, int currElem)
+        {
+            Console.WriteLine(arr[currElem]);
+            if (currElem < arr.Length - 1)
+                printViaRecursion(arr, currElem + 1);
+        }
+    }
+}
+   
