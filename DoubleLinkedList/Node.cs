dae6197588835b169062fa5524c0ca9ea822bd57
swathibylla <swathibylla@gmail.com>
2023-10-29 23:02:31 +0530

    files

---
+++ DoubleLinkedList/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phase1sectionn4._7
+{
+    internal class Node
+    {
+        private string data;
+        private Node next = null;
+        private Node prev = null;
+
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+
+        public Node Prev
+        {
+            get { return this.prev; }
+            set { this.prev = value; }
+        }
+    }
+
+}
+
---
+++ DoubleLinkedList/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phase1sectionn4._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            DoubleLinkedList list = new DoubleLinkedList();
+            Node n = new Node();
+            n.Data = "root";
+            list.add(n);
+            for (int i = 0; i < 10; i++)
+            {
+                Node node = new Node();
+                node.Data = Convert.ToString(i);
+                list.add(node);
+            }
+
+            Console.WriteLine("length of singly linked list=" + list.getLengthOfList());
+            Console.WriteLine("Traversing forward..");
+            n = list.getRoot();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getNext();
+            }
+
+
+            Console.WriteLine("Traversing backwards..");
+            n = list.getPrev();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getPrev();
+            }
+
+        }
+    }
+}
+     
