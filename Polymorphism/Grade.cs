31524d5f50a2c7cdd2b56c9ea26377058e92050d
swathibylla <swathibylla@gmail.com>
2023-10-29 22:52:37 +0530

    files

---
+++ Polymorphism/Grade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class BasicGradeCard
+    {
+        protected int totalMarks;
+
+        public BasicGradeCard()
+        {
+            totalMarks = 0;
+        }
+
+        public virtual int getTotalMarks()
+        {
+            return totalMarks;
+        }
+    }
+
+}
+
