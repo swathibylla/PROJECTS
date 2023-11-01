ddea44eb113e5f1e45a45be261599ea54ab797b0
swathibylla <swathibylla@gmail.com>
2023-11-01 11:42:03 +0530

    files

---
+++ multithread/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace multithread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CurrentThreadExample();
+            Thread t1 = Thread.CurrentThread;
+            t1.Name = "This method is main method is running";
+            Console.WriteLine(t1.Name);
+            Console.WriteLine($"Managed Main Thread id = {t1.ManagedThreadId}");//1
+            Console.Read();
+
+            //it can any method which takes object(any data type) and returns void
+            ThreadStart del = new ThreadStart(Program.Print);
+            //Thread t = new Thread(new ThreadStart(Program.Print));
+            Thread t = new Thread(del);
+            Thread tobj = new Thread(Program.Greet);
+
+
+            //Thread t2 = new Thread(new ThreadStart(Program.Greet));
+
+            t.Start();
+            Console.WriteLine("----------");
+            Console.WriteLine($"Managed  Print Thread id={t.ManagedThreadID}");//3
+
+            t2.Start();
+            Console.ReadLine();
+        }
+    }
+}
