02ff5c9219a0087db6396f9c6c0f0e85bb6af877
swathibylla <swathibylla@gmail.com>
2023-11-01 15:21:52 +0530

    files

---
+++ sum/sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            int sum = 0;
+
+            foreach(int number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine("The sum of all elements:"+sum);
+        }
+    }
+}
