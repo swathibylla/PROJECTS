03cb2b5a042703a86a43674273af8cd914a568bc
swathibylla <swathibylla@gmail.com>
2023-11-23 10:45:29 +0530

    my files

---
+++ Rainbow/Rainbow/data/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Rainbow.data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Student> students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Class> Classes { get; set; }
+
+    }
+}
